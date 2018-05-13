75eccb031565154dc30fd9377cc028670cebefc2
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-05-13 15:25:52 -0500

    modify port

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const PORT = process.env.PORT || 5000
 const app = express()
+
 
 app.use(express.static('public'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
