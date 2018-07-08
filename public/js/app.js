4875064b7311b1f6da378906fdc17b844fba83c5
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-08 12:22:14 -0500

    add interactivity to arrows

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,30 @@
-var expanders = Array.from(document.getElementsByClassName('expand'));
+"use strict";
+
+var expanders = Array.from( document.getElementsByClassName('expand'));
+
+var arrows = document.getElementsByClassName('arrows');
+
 
 function unhide() {
   this.nextElementSibling.classList.toggle('hidden');
 }
 
-function accordion() {
-  expanders.forEach(element => element.addEventListener('click', unhide));
+function toggleArrow(){
+  if (this.firstElementChild.style.cssText) {
+    this.firstElementChild.style.cssText = null;
+  } else{
+    this.firstElementChild.style.transform = 'rotate(-90deg)';
+  }
 }
 
-accordion();
+function accordion() {
+}
+expanders.forEach(element => element.addEventListener('click', unhide));
+expanders.forEach(element => element.addEventListener('click', toggleArrow));
+
+
+// accordion();
+
+// style.transform = "rotate(0deg)";
+// style.cssText = 'transform: rotate(-90deg);';
+// style.cssText = null;
